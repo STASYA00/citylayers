fd497a47e1e42cc1d57d8e117490d93aaf5fd417
ArcherDilunaire <aurelrichard00@gmail.com>
2024-05-13 00:42:47 +0200

    implementing steps logic in  datacollection page

--- resources/views/addPin.blade.php
+++ resources/views/addPin.blade.php
@@ -11,17 +11,17 @@
 
 <div class="flex-wrapper" x-data>
     <section x-show="$store.data.step.current == 0">
-        <h1 class="title">What do you want to pin to the map?</h1>
-        <p class="text"> Begin by adding a photo of the place you want to share. Then, follow a few steps to share
+        <h1>What do you want to pin to the map?</h1>
+        <p> Begin by adding a photo of the place you want to share. Then, follow a few steps to share
             your thoughts about this place with others. Once completed, your contribution will be displayed on the map
             for
             everyone to see.</p>
-        <button class="primary-button" x-bind:disabled="!$store.data.allowedLocation" @click="$store.data.nextStep()">
+        <button class="primary-button" :disabled="!$store . data . allowedLocation" @click="$store.data.nextStep()">
             Let's
             get started </button>
         <label for="img-uploader">
             <div class="img-container">
-                <input id="img-uploader" @change="$store.data.setImage(event)" type="file" class="" accept=".jpg, .png">
+                <input id="img-uploader" @change="$store.data.setImage(event)" type="file" accept=".jpg, .png">
                 <img id="img-preview">
                 <div class="img-text" x-show="!$store.data.place_data['place_image']"> <span>+</span> Add a picture
                 </div>
@@ -31,8 +31,21 @@
         </label>
     </section>
     <section x-cloak x-show="$store.data.step.current > 0">
-        <h1 class="title">hello world</h1>
-        <button class="secondary-button" @click="$store.data.prevStep()"> prev </button>
+        <h1 class="question" x-html="$store.data.copy.questions[$store.data.step.current-1]"></h1>
+        <input type="range" id="slider" name="slider" min="0" max="1" step="0.1" />
+        <h2 class="subquestion" x-html="$store.data.copy.subquestions[$store.data.step.current-1]"></h2>
+        <div class="tags-container">
+            <template x-for="tag in $store.data.tags[$store.data.step.current-1]">
+                <li x-text="tag"></li>
+            </template>
+        </div>
+        <footer>
+            <div class="steps" x-text="'step: ' + $store.data.step.current "></div>
+            <div class="nav-buttons">
+                <button class="back-button" @click="$store.data.prevStep()"> back </button>
+                <button class="next-button" @click="$store.data.nextStep()"> next </button>
+            </div>
+        </footer>
     </section>
 </div>
 
@@ -57,6 +70,26 @@
                 place_img: null,
                 comment: null,
             },
+
+            copy: {
+                questions: [
+                    "How would you rate the <u>beauty</u> of this space?",
+                    "What do you think of the <u>sounds</u> around you?",
+                    "How would you rate the movement around this place?"
+                ],
+                subquestions: [
+                    "Which features of beauty are you rating?",
+                    "Which sounds are you rating?",
+                    "Which aspects of movement are you rating?"
+                ],
+                tagsDescription: "Select one or more tags below"
+            },
+
+            tags: [
+                ["buildings", "landmarks"],
+                ["water", "wind"],
+                ["walking", "cycling"]
+            ],
 
             nextStep() {
                 if (this.step.current < this.step.length) this.step.current += 1;
