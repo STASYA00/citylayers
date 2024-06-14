d2ec9639b9a4f53d322b720fb80f780f4c3378df
STASYA00 <0.0stasya@gmail.com>
2024-06-14 14:53:40 +0200

    add input to vite config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,10 @@ export default defineConfig({
     },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css',
+                'resources/css/*.css',
+                'resources/css/*.js',
+                'resources/js/app.js'],
             refresh: true,
         }),
     ],
